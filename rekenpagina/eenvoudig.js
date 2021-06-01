fdf0f017418839e1f5718b41763a6ea84ca74d6a
RobbertBons <74967915+RobbertBons@users.noreply.github.com>
2021-06-01 14:50:41 +0200

    macht reeks

--- rekenpagina/eenvoudig.js
+++ rekenpagina/eenvoudig.js
@@ -1,9 +1,9 @@
-  
+
 function genereerTafel() {
     var vermenigvuldigtal = document.getElementById("tafel-vermenigvuldigtal").value; // De invoer van de gebruiker
     var maxVermenigvuldiger = document.getElementById("tafel-max-vermenigvuldiger").value;
     var antwoordVeld = document.getElementById("tafel-antwoord");
-    
+
     antwoordVeld.value = ""; // We beginnen elke reeks met een leeg antwoord veld
 
     for(var vermenigvuldiger = 1; vermenigvuldiger <= maxVermenigvuldiger; vermenigvuldiger++) {
@@ -19,63 +19,47 @@ function genereerTafel() {
 
 function breuken() {
     var maxnoemer = document.getElementById("max_noemer").value;
-    
+
     var antwoordVeld = document.getElementById("breuk-antwoord");
-    
+
     antwoordVeld.value = "";
 
     for(var noemer = 1; noemer <= maxnoemer; noemer++){
         var quotient = 1 / noemer;
         antwoordVeld.value += "1 / " + noemer + " = " + quotient + " \n ";
-        
+
     }
     return false;
 }
+
+function machten() {
+   var maxGrondgetal = document.getElementById("machten-grondgetal").value;
+
+   var antwoordVeld = document.getElementById("machten-antwoord");
+
+   antwoordVeld.value = "";
+
+   for(var grondgetal = 1; grondgetal <= maxgrondgetal; grondgetal++){
+       var product = grondgetal * exponent;
+       var kwadraat = grondgetal + "" + exponent + "=" + product;
+       antwoordVeld.value += kwadraat + "\n";
+   }
+   return false;
+}
+
 
 
 function kwadraten() {
     var maxnoemer = document.getElementById("kwadraten-grondgetal").value;
-    
-    var antwoordVeld = document.getElementById("kwadraten-antwoord");
-    
+
+    var antwoordVeld = document.getElementById("kwadraat-antwoord");
+
     antwoordVeld.value = "";
 
     for(var noemer = 1; noemer <= maxnoemer; noemer++){
         var quotient = 1 / noemer;
         antwoordVeld.value += "1 / " + noemer + " = " + quotient + " \n ";
-        
+
     }
     return false;
 }
-
-
-
-
-
-
-
-
-
-function power(base, exponent) {
-    var result = 1;
-        for(var count = 0;count < exponent; count++)
-            result *= base
-            return result;
-        }
-
-    power(2, 10);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
